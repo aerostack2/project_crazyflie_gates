d3f50a306ca44deec9f1b1206866cc9c368421e8
pariaspe <pedro.ariasp@upm.es>
2024-07-29 13:18:27 +0200

    entrypoint updated

--- launch_as2.bash
+++ launch_as2.bash
@@ -7,109 +7,118 @@ usage() {
     echo "      -e: estimator_type, choices: [ground_truth, raw_odometry, mocap_pose]"
     echo "      -r: record rosbag"
     echo "      -t: launch keyboard teleoperation"
-    echo "      -n: drone namespace, default is cf0"
+    echo "      -v: open rviz, choices: [true | false]"
 }
 
 # Arg parser
-while getopts "se:mrtn" opt; do
-  case ${opt} in
-    s )
-      simulated="true"
-      ;;
-    m )
-      swarm="true"
-      ;;
-    e )
-      estimator_plugin="${OPTARG}"
-      ;;
-    r )
-      record_rosbag="true"
-      ;;
-    t )
-      launch_keyboard_teleop="true"
-      ;;
-    n )
-      drone_namespace="${OPTARG}"
-      ;;
-    \? )
-      echo "Invalid option: -$OPTARG" >&2
-      usage
-      exit 1
-      ;;
-    : )
-      if [[ ! $OPTARG =~ ^[swrt]$ ]]; then
-        echo "Option -$OPTARG requires an argument" >&2
-        usage
-        exit 1
-      fi
-      ;;
-  esac
+while getopts "se:mrtv" opt; do
+    case ${opt} in
+        s )
+            simulated="true"
+            ;;
+        m )
+            swarm="true"
+            ;;
+        e )
+            estimator_plugin="${OPTARG}"
+            ;;
+        r )
+            record_rosbag="true"
+            ;;
+        t )
+            launch_keyboard_teleop="true"
+            ;;
+        v )
+            rviz="true"
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        : )
+            if [[ ! $OPTARG =~ ^[swrt]$ ]]; then
+                echo "Option -$OPTARG requires an argument" >&2
+                usage
+                exit 1
+            fi
+            ;;
+    esac
 done
-
-source utils/tools.bash
 
 # Shift optional args
 shift $((OPTIND -1))
 
 ## DEFAULTS
-simulated=${simulated:="false"}  # default ign_gz
-if [[ ${simulated} == "false" && -z ${estimator_plugin} ]]; then
-  echo "Error: when -s is false, -e argument must be set" 1>&2
-  usage
-  exit 1
+estimator_plugin=${estimator_plugin:="raw_odometry"}
+simulated=${simulated:="false"}
+# If simulated, estimator_plugin is ground_truth
+if [[ ${simulated} == "true" ]]; then
+    estimator_plugin="ground_truth"
 fi
-
 swarm=${swarm:="false"}
-estimator_plugin=${estimator_plugin:="ground_truth"}  # default ign_gz
+rviz=${rviz:="false"}
 record_rosbag=${record_rosbag:="false"}
 launch_keyboard_teleop=${launch_keyboard_teleop:="false"}
-drone_namespace=${drone_namespace:="cf"}
 
-if [[ ${swarm} == "true" ]]; then
-  num_drones=2
-  simulation_config="sim_config/world_swarm.json"
-else
-  num_drones=1
-  simulation_config="sim_config/world.json"
+simulation_config="real_config/swarm_config_file.yaml"
+if [[ ${simulated} == "true" ]]; then
+    simulation_config="sim_config/world.json"
+    if [[ ${swarm} == "true" ]]; then
+        simulation_config="sim_config/world_swarm.json"
+    fi
 fi
 
-# Generate the list of drone namespaces
-drone_ns=()
-for ((i=0; i<${num_drones}; i++)); do
-  drone_ns+=("$drone_namespace$i")
-done
+# Fron swarm config, get the list of drone namespaces
+drone_namespaces=$(python3 utils/get_drones.py ${simulation_config})
+drone_namespaces_list=($(echo $drone_namespaces | tr ':' ' '))
 
-for ns in "${drone_ns[@]}"
+for ns in "${drone_namespaces_list[@]}"
 do
-  if [[ ${ns} == ${drone_ns[0]} ]]; then
-    base_launch="true"
-  else
     base_launch="false"
-  fi 
+    if [[ ${ns} == ${drone_namespaces_list[0]} ]]; then
+        base_launch="true"
+    fi 
 
-  tmuxinator start -n ${ns} -p utils/aerostack2.yml drone_namespace=${ns} base_launch=${base_launch}  estimator_plugin=${estimator_plugin} simulation=${simulated} simulation_config=${simulation_config} &
-  wait
+    tmuxinator start -n ${ns} -p utils/aerostack2.yml \
+        drone_namespace=${ns} \
+        base_launch=${base_launch} \
+        estimator_plugin=${estimator_plugin} \
+        simulation=${simulated} \
+        simulation_config=${simulation_config} &
+    wait
 done
 
 if [[ ${estimator_plugin} == "mocap_pose" ]]; then
-  tmuxinator start -n mocap -p utils/mocap4ros2.yml &
-  wait
+    tmuxinator start -n mocap -p utils/mocap4ros2.yml &
+    wait
 fi
 
 if [[ ${record_rosbag} == "true" ]]; then
-  tmuxinator start -n rosbag -p utils/rosbag.yml drone_namespace=$(list_to_string "${drone_ns[@]}") &
-  wait
+    tmuxinator start -n rosbag -p utils/rosbag.yml \
+        drones=${drone_namespaces} &
+    wait
 fi
 
 if [[ ${launch_keyboard_teleop} == "true" ]]; then
-  tmuxinator start -n keyboard_teleop -p utils/keyboard_teleop.yml simulation=true drone_namespace=$(list_to_string "${drone_ns[@]}") &
-  wait
+    drone_namespaces_comma=$(echo $drone_namespaces | tr ':' ',')
+    tmuxinator start -n keyboard_teleop -p utils/keyboard_teleop.yml \
+        simulation=${simulated} \
+        drone_namespaces=${drone_namespaces_comma} &
+    wait
 fi
 
 if [[ ${simulated} == "true" ]]; then
-  tmuxinator start -n gazebo -p utils/gazebo.yml simulation_config=${simulation_config} &
+    tmuxinator start -n gazebo -p utils/gazebo.yml \
+        simulation_config=${simulation_config} &
+    wait
+fi
+
+if [[ ${rviz} == "true" ]]; then
+  tmuxinator start -p utils/rviz.yml \
+      simulation=${simulated} \
   wait
 fi
 
 # Attach to tmux session ${drone_ns[@]}, window 0
-tmux attach-session -t ${drone_ns[0]}:mission
+tmux attach-session -t ${drone_namespaces_list[0]}:mission
